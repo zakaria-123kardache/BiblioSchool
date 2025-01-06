f0c0e245445fe2d2f20ff54d027f9029a411a0e0
unknown <zz240614@gmail.com>
2025-01-06 10:55:14 +0100

    Creat Class Geron whit the method

---
+++ src/class/Geron.php
@@ -0,0 +1,32 @@
+<?php 
+require_once('./Utilisateur.php');
+
+class Geron extends Utilisateur {
+
+
+    public function CreatLivre(){
+
+    }
+    
+    public function EditLivre(){
+
+    }
+
+    public function ValidationReservation(){
+
+    }
+
+    public function RejectReservation(){
+
+    }
+
+};
+
+
+
+
+
+
+
+
+?> 
