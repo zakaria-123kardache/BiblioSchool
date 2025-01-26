22fa93b1407f61c68220b7caffb2ab9a59648ecc
unknown <zz240614@gmail.com>
2025-01-26 18:31:05 +0100

    Created Connexion class to manage DB connxion using PDO with Singleton pattern

---
+++ app/Core/Connexion.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\Core ;
+
+use PDO;
+use PDOException;
+
+class Connexion {
+
+    private static $servername = "localhost"; 
+    private static $username = "root"; 
+    private static $password = ""; 
+    private static $db = "biblioschool";
+    private static $connexion ; 
+    private static $instance ; 
+    public static $counter = 0 ; 
+
+
+    public function __construct()
+    {
+        if (!self::$connexion)
+        {
+            try {
+                self::$connexion = new PDO(
+                    "mysql:host=" . self::$servername . 
+                    ";dbname=" . self::$db . 
+                    ";charset=UTF8",
+                    self::$username,
+                    self::$password
+                );
+
+                self::$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("connexion failed" . $e->getMessage());
+            }
+        }
+    }
+
+    public static function getInstance()
+    {
+        if(!self::$instance){
+            self::$instance = new Connexion();
+            self::$counter ++ ; 
+        }
+
+        return self::$instance ; 
+    } 
+
+    public function getConnexion ()
+    {
+        return  self::$connexion; 
+       
+    }
+
+}
