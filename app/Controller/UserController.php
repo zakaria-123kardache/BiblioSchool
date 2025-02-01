46bed698b9b343da90623ca2da376561a18ffa02
unknown <zz240614@gmail.com>
2025-02-01 12:47:53 +0100

    implemented UserController Whit Json res

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -1,7 +1,60 @@
 <?php
 
+use App\Models\User;
 
-class UserControoler {
+class UserController {
+
+
+    protected $user ; 
+
+    public function __construct()
+    {
+        $this->user = new User();
+    }
+
+    public function create($data)
+    {
+        $result =$this->user->create($data);
+
+        if ($result){
+            $response = ['ssuccessMessage' => 'User Creted Seccessfl'];
+        } else {
+            $response = ['errorMessage' => 'User Not Created'];
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+
+    }
+
+    public function update ($id,$data)
+    {
+        $result = $this->user->update($id, $data);
+
+        if ($result){
+            $response = ['ssuccessMessage' => 'User Creted Seccessfl'];
+        } else {
+            $response = ['errorMessage' => 'User Not Created'];
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+
+    }
+
+    public function delete($id)
+    {
+        $this->user->delete($id);
+        header('Location: ');
+    }
+
+    public function find($id)
+    {
+        $user = $this->user->find($id);
+        return $user ; 
+
+    
+    }
 
     
 }
